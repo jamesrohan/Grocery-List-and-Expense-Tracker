07412fffb67628ef12ac3183e0ecd1062f74d511
jamesrohan <james.reckon@gmail.com>
2017-11-14 08:30:53 -0500

    Added MyGroups Activity as well, Fixed Bugs, Use Empty Activity not Blank
    Added MyGroups Activity as well, Fixed Bugs, Use Empty Activity not
    Blank

--- AndroidStudioCode/G6_GroceryList/app/src/main/java/com/example/gsu_g6/g6_grocerylist/MyLists_Activity.java
+++ AndroidStudioCode/G6_GroceryList/app/src/main/java/com/example/gsu_g6/g6_grocerylist/MyLists_Activity.java
@@ -1,11 +1,12 @@
 package com.example.gsu_g6.g6_grocerylist;
 
-import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MyLists_Activity extends ListActivity {
+public class MyLists_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +16,19 @@ public class MyLists_Activity extends ListActivity {
         initButton();
     }
 
-
     private void initButton(){
-        Button AddList = (Button) findViewById(R.id.Button_AddList);
-        Button OpenGroups = (Button) findViewById(R.id.Button_OpenGroups);
+        Button AddList = (Button) findViewById(R.id.Button_AddList1);
+        Button OpenGroups = (Button) findViewById(R.id.Button_OpenGroups1);
+
+        OpenGroups.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent intent = new Intent(MyLists_Activity.this, MyGroups_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
 }
