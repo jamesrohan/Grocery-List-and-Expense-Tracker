f776f6cf34124fa2169d1c3a0e5473cc5aec84a5
jamesrohan <james.reckon@gmail.com>
2017-11-13 21:01:23 -0500

    Created Project
    Added Log In Screen
    
    Working on Lists Page
    
    Used Android 5.1 API 22

---
+++ AndroidStudioCode/G6_GroceryList/app/src/main/java/com/example/gsu_g6/g6_grocerylist/MainLogInActivity.java
@@ -0,0 +1,13 @@
+package com.example.gsu_g6.g6_grocerylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_log_in);
+    }
+}
---
+++ AndroidStudioCode/G6_GroceryList/app/src/main/java/com/example/gsu_g6/g6_grocerylist/MyLists_Activity.java
@@ -0,0 +1,14 @@
+package com.example.gsu_g6.g6_grocerylist;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyLists_Activity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_lists_);
+    }
+}
